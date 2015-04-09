c2c90787745f61b74e3f4b1aeb4f08f1c6ee6439
Eirik Marthinsen <eirik@petrell.no>
2015-04-10 00:13:59 +0200

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+"""Install the polar2tcx script
+
+Run
+
+  python setup.py install
+
+to install. Then you can run the script from everywhere by typing
+
+  polar2tcx.py
+
+"""
+
+from distutils.core import setup
+
+setup(name='Polar2tcx',
+      version='1.0',
+      description='Convert polar xml and gpx files to tcx',
+      author='Eirik Marthinsen, Frank Meffert',
+      author_email='eirikma@gmail.com',
+      url='https://github.com/marthinsen/polar2tcx',
+      scripts=['polar2tcx.py'])
