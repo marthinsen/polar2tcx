e12ce4270dbe9fb1db46855e1392c43d156eaf78
Eirik Marthinsen <eirik@petrell.no>
2015-07-15 23:05:51 +0200

    Add unittest for ceilTime

---
+++ tests/test_polar2tcx.py
@@ -0,0 +1,14 @@
+import unittest
+import datetime
+
+import polar2tcx
+
+class Polar2tcxTest(unittest.TestCase):
+
+    def test_ceilTime(self):
+        t = datetime.datetime(2015, 07, 15)
+        self.assertEqual(t, polar2tcx.ceilTime(t))
+
+        t = datetime.datetime(2015, 07, 15, second=59, microsecond=1)
+        tr = datetime.datetime(2015, 07, 15, minute=1)
+        self.assertEqual(tr, polar2tcx.ceilTime(t))
